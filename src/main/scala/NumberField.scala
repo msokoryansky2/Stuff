42ef46ec7895e2755cf3fba5ad8fec50bb4c8aa9
M S <msokoryansky@yahoo.com>
2017-09-19 20:44:20 -0400

    Test commit to see if GitHub integration is working again

--- src/main/scala/NumberField.scala
+++ src/main/scala/NumberField.scala
@@ -2,7 +2,7 @@ import util.Properties
 
 /**
   * Class representing a two-dimensional "field" of integers.
-  * Each element of the inner array is a row in the field.
+  * Each element of the inner array is a row in the field of integers.
   * @param field represents two-dimension field of numbers.
   */
 class NumberField (field: Array[Array[Int]]) {
