c500edd7805a579ac3b29b5caf658d04de1e84e2
M S <msokoryansky@yahoo.com>
2017-10-06 12:46:03 -0400

    with an unnecessary comment thrown in

--- src/main/scala/Quine.scala
+++ src/main/scala/Quine.scala
@@ -4,8 +4,10 @@ object Quine extends App {
       |Object Quine extends App {
       |  val s =
       |    ""%2$s%1$s%2$s""
+      |  // This is a totally unnecessary comment
       |  println(s.stripMargin.format(s, "\""))
       |}
     """
+  // This is a totally unnecessary comment
   println(s.stripMargin.format(s, "\""))
 }
