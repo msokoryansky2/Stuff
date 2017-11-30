e2f373424946373db0f17d0db497547b1aeec08b
M S <msokoryansky@yahoo.com>
2017-11-30 18:41:42 -0500

    re-create the original sample Quine step-by-step + my own Quine from scratch

--- src/main/scala/Quine.scala
+++ src/main/scala/Quine.scala
@@ -1,7 +1,7 @@
 object Quine extends App {
   val s =
     """
-      |Object Quine extends App {
+      |object Quine extends App {
       |  val s =
       |    ""%2$s%1$s%2$s""
       |  // This is a totally unnecessary comment
@@ -11,3 +11,17 @@ object Quine extends App {
   // This is a totally unnecessary comment
   println(s.stripMargin.format(s, "\""))
 }
+
+object Quine2 extends App {
+  val s =
+    """
+      |object Quine2 extends App {
+      |  val s =
+      |    ""%2$s%1$s%2$s""
+      |  println(s.stripMargin.format(s, "\""))
+      |}
+    """
+    println(s.stripMargin.format(s, "\""))
+}
+
+object Quine3 extends App { val s = "\\"; val q = "\""; val p = "object Quine3 extends App { val s = %1$s%3$s%3$s%1$s; val q = %1$s%3$s%1$s%1$s; val s = %1$s%2$s%1$s; println(p.format(q, p, s)) }"; println(p.format(q, p, s)) }
