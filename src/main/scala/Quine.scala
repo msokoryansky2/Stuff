ab45d2a0ec943b8e7eee360e25e243ac56d16e43
M S <msokoryansky@yahoo.com>
2017-11-30 18:56:53 -0500

    step-by-step building a Quine

--- src/main/scala/Quine.scala
+++ src/main/scala/Quine.scala
@@ -25,3 +25,13 @@ object Quine2 extends App {
 }
 
 object Quine3 extends App { val s = "\\"; val q = "\""; val p = "object Quine3 extends App { val s = %1$s%3$s%3$s%1$s; val q = %1$s%3$s%1$s%1$s; val p = %1$s%2$s%1$s; println(p.format(q, p, s)) }"; println(p.format(q, p, s)) }
+
+
+
+object Quine4a extends App { val p = "object Quine4a extends App { val p = %1$s; println(p.format(p))}"; println(p.format(p))}
+
+object Quine4b extends App { val p = "object Quine4b extends App { val p = \"%1$s\"; println(p.format(p))}"; println(p.format(p))}
+
+object Quine4c extends App { val q = "\""; val p = "object Quine4c extends App { val q = \"%2$s\"; val p = %2$s%1$s%2$s; println(p.format(p, q))}"; println(p.format(p, q))}
+
+object Quine4d extends App { val s = "\\"; val q = "\""; val p = "object Quine4d extends App { val s = %2$s%3$s%3$s%2$s; val q = %2$s%3$s%2$s%2$s; val p = %2$s%1$s%2$s; println(p.format(p, q, s))}"; println(p.format(p, q, s))}
