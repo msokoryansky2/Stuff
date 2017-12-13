e0f126246d2b502f934491d4f53137cf0294276b
M S <msokoryansky@yahoo.com>
2017-12-13 18:20:06 -0500

    starting on MikeLinkedList class

--- src/test/scala/TestPrime.scala
+++
@@ -1,7 +0,0 @@
-import org.scalatest.FunSuite
-
-class TestPrime extends FunSuite {
-  test("primes should return 2, 3, 5, 7, 11, 13, ...") {
-    assert(Prime.primes.take(10).toList === List(2, 3, 5, 7, 11, 13, 17, 19, 23, 29))
-  }
-}
